f93832beff1e2e14a05c8a0ba94f677c73e832b0
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-03-09 12:36:42 +0200

    updated app.py

--- src/app.py
+++ src/app.py
@@ -26,5 +26,9 @@ def html():
 def math():
     return render_template("math.html")
 
+@app.route("/size")
+def size():
+    return render_template("size.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
